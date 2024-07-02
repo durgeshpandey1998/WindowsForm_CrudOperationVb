67f8431027c60d94d767179a155ee8ef629b7201
durgeshpandey1998 <durgeshpandey239@gmail.com>
2024-07-02 15:27:17 +0530

    Add project files.

---
+++ CrudOperationVb/Form1.Designer.vb
@@ -0,0 +1,284 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        DateTimePicker1 = New DateTimePicker()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        Label6 = New Label()
+        Label7 = New Label()
+        DateTimePicker2 = New DateTimePicker()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        DataGridView1 = New DataGridView()
+        Button3 = New Button()
+        TextBox5 = New TextBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(102, 270)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(200, 23)
+        DateTimePicker1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(15, 34)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(61, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "FirstName"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(15, 82)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(60, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "LastName"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(15, 133)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(45, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "Gender"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(15, 174)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(36, 15)
+        Label4.TabIndex = 4
+        Label4.Text = "Email"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(15, 217)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(49, 15)
+        Label5.TabIndex = 5
+        Label5.Text = "Contact"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(102, 131)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(51, 19)
+        RadioButton1.TabIndex = 6
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Male"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(159, 131)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(63, 19)
+        RadioButton2.TabIndex = 7
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Female"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(242, 133)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(60, 19)
+        RadioButton3.TabIndex = 8
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Others"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(15, 270)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(72, 15)
+        Label6.TabIndex = 9
+        Label6.Text = "Joining Date"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(15, 329)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(75, 15)
+        Label7.TabIndex = 10
+        Label7.Text = "Date Of Birth"
+        ' 
+        ' DateTimePicker2
+        ' 
+        DateTimePicker2.Location = New Point(102, 321)
+        DateTimePicker2.Name = "DateTimePicker2"
+        DateTimePicker2.Size = New Size(200, 23)
+        DateTimePicker2.TabIndex = 11
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(102, 34)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 12
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(102, 79)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 14
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(102, 166)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 16
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(102, 214)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 18
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(13, 383)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 19
+        Button1.Text = "Save"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(127, 383)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 20
+        Button2.Text = "Reset"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(334, 79)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(1065, 432)
+        DataGridView1.TabIndex = 21
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(450, 26)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(114, 23)
+        Button3.TabIndex = 22
+        Button3.Text = "GetData"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(274, 37)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(100, 23)
+        TextBox5.TabIndex = 23
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1451, 682)
+        Controls.Add(TextBox5)
+        Controls.Add(Button3)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(DateTimePicker2)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(DateTimePicker1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox5 As TextBox
+
+End Class
---
+++ CrudOperationVb/Form1.vb
@@ -0,0 +1,347 @@
+﻿Imports System.IO
+Imports System.Data.SQLite
+Imports System.Net
+Imports System.Globalization
+
+Public Class Form1
+    Private connectionString As String = "Data Source=G://Durgesh-Learning//Asp.net//CrudOperationVb//Employee.db"
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox5.Visible = False
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'GetDataForId()
+        Dim id As Integer
+        id = TextBox5.Text
+        Dim firstName = Trim(TextBox1.Text)
+        Dim lastName = Trim(TextBox2.Text)
+        Dim gender = If(RadioButton1.Checked, Trim(RadioButton1.Text),
+              If(RadioButton2.Checked, Trim(RadioButton2.Text),
+                 Trim(RadioButton3.Text)))
+
+        Dim email = Trim(TextBox3.Text)
+        Dim contact = Trim(TextBox4.Text)
+        Dim joiningDate = Trim(DateTimePicker1.Text)
+        Dim dateOfBirth = Trim(DateTimePicker2.Text)
+        ValidateForm(firstName, lastName, email, gender, contact, joiningDate, dateOfBirth)
+        CreateDatabase(connectionString)
+        CreateTable(connectionString)
+        If id > 0 Then
+            UpdateData(id, firstName, lastName, email, gender, contact, joiningDate, dateOfBirth)
+        Else
+            InsertData(firstName, lastName, email, gender, contact, joiningDate, dateOfBirth)
+        End If
+
+    End Sub
+    Private Sub CreateTable(databasePath As String)
+        Dim sql = "CREATE TABLE IF NOT EXISTS AddressDetail (
+                                AddressId INTEGER PRIMARY KEY,
+                                Street TEXT,
+                                Area TEXT,
+                                Zipcode TEXT,
+                                City TEXT,
+                                State TEXT,
+                                Country TEXT
+                            );"
+        Dim sqlEmployee = "CREATE TABLE IF NOT EXISTS EmployeeDetail (
+                                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        FirstName TEXT,
+                                        LastName TEXT,
+                                        Email TEXT,
+                                        Contact TEXT,
+                                        Gender TEXT,
+                                        Dob TEXT  ,
+                                        JoiningDate TEXT
+                                    ); "
+
+        Using connection As New SQLiteConnection(connectionString)
+            connection.Open()
+
+            Using command As New SQLiteCommand(sqlEmployee, connection)
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+    Private Sub CreateDatabase(databasePath As String)
+        databasePath = "G://Durgesh-Learning//Asp.net//CrudOperationVb//Employee.db"
+        If Not File.Exists(databasePath) Then
+            'databasePath = connectionString
+            File.Create(databasePath).Close()
+        End If
+    End Sub
+    Private Function GetDataForId() As Integer
+        Dim lastId As Integer = 0
+
+        Dim sql As String = "SELECT Id FROM EmployeeDetail ORDER BY Id DESC LIMIT 1"
+
+        Using connection As New SQLiteConnection(connectionString)
+            connection.Open()
+
+            Using command As New SQLiteCommand(sql, connection)
+                Dim result = command.ExecuteScalar()
+                If result IsNot Nothing AndAlso Not DBNull.Value.Equals(result) Then
+                    lastId = Convert.ToInt32(result)
+                    lastId += 1 ' Increment the lastId by 1
+                End If
+            End Using
+        End Using
+
+        ' Now lastId holds the incremented value of the last Id from the table
+        Return lastId
+    End Function
+
+    Private Sub InsertData(firstName As String, lastName As String, email As String, gender As String, contact As String, joiningDate As String, dob As String)
+        GetDataForId()
+        Dim sql = "INSERT INTO EmployeeDetail (FirstName, LastName, Email, Contact, Gender, Dob, JoiningDate ) VALUES (@firstName, @lastName, @email, @contact, @gender, @dob, @joiningDate)"
+
+        Try
+            Dim id As Integer = GetDataForId()
+            Using connection As New SQLiteConnection(connectionString)
+                connection.Open()
+
+                Using command As New SQLiteCommand(sql, connection)
+                    command.Parameters.AddWithValue("@Id", id)
+                    command.Parameters.AddWithValue("@firstName", firstName)
+                    command.Parameters.AddWithValue("@lastName", lastName)
+                    command.Parameters.AddWithValue("@email", email)
+                    command.Parameters.AddWithValue("@contact", contact)
+                    command.Parameters.AddWithValue("@gender", gender)
+                    command.Parameters.AddWithValue("@dob", dob)
+                    command.Parameters.AddWithValue("@joiningDate", joiningDate)
+
+                    command.ExecuteNonQuery()
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox("Error inserting data: " & ex.Message, MsgBoxStyle.Critical, "Error")
+        End Try
+        'MsgBox("Registration successful!", MsgBoxStyle.Information, "Information")
+        'ClearTextBoxes()
+    End Sub
+
+    Private Sub UpdateData(id As Integer, firstName As String, lastName As String, email As String, gender As String, contact As String, joiningDate As String, dob As String)
+        Dim sql As String = "UPDATE EmployeeDetail SET FirstName = @firstName, LastName = @lastName, Email = @email,
+                         Contact = @contact, Gender = @gender, Dob = @dob, JoiningDate = @joiningDate
+                         WHERE Id = @id"
+
+        Try
+            Using connection As New SQLiteConnection(connectionString)
+                connection.Open()
+
+                Using command As New SQLiteCommand(sql, connection)
+                    command.Parameters.AddWithValue("@id", id)
+                    command.Parameters.AddWithValue("@firstName", firstName)
+                    command.Parameters.AddWithValue("@lastName", lastName)
+                    command.Parameters.AddWithValue("@email", email)
+                    command.Parameters.AddWithValue("@contact", contact)
+                    command.Parameters.AddWithValue("@gender", gender)
+                    command.Parameters.AddWithValue("@dob", dob)
+                    command.Parameters.AddWithValue("@joiningDate", joiningDate)
+
+                    command.ExecuteNonQuery()
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox("Error updating data: " & ex.Message, MsgBoxStyle.Critical, "Error")
+        End Try
+        MsgBox("Update successful!", MsgBoxStyle.Information, "Information")
+    End Sub
+
+    Private Sub ValidateForm(firstName As String, lastName As String, email As String, gender As String, contact As String, joiningDate As String, dateOfBirth As String)
+        If Not IsValidName(firstName) Then
+            MsgBox("Please enter a valid first name (letters only).", MsgBoxStyle.Exclamation, "Warning")
+            Return
+        End If
+
+        If Not IsValidName(lastName) Then
+            MsgBox("Please enter a valid last name (letters only).", MsgBoxStyle.Exclamation, "Warning")
+            Return
+        End If
+
+        If Not IsValidEmail(email) Then
+            MsgBox("Please enter a valid email address.", MsgBoxStyle.Exclamation, "Warning")
+            Return
+        End If
+
+        If Not IsValidPhoneNumber(contact) Then
+            MsgBox("Please enter a valid phone number (up to 10 digits).", MsgBoxStyle.Exclamation, "Warning")
+            Return
+        End If
+    End Sub
+    Private Function IsValidName(name As String) As Boolean
+        Return Not String.IsNullOrWhiteSpace(name) AndAlso System.Text.RegularExpressions.Regex.IsMatch(name, "^[a-zA-Z]+$")
+    End Function
+
+
+    Private Function IsValidEmail(email As String) As Boolean
+        Return Not String.IsNullOrWhiteSpace(email) AndAlso System.Text.RegularExpressions.Regex.IsMatch(email, "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")
+    End Function
+
+    Private Function IsValidPhoneNumber(phone As String) As Boolean
+        Return Not String.IsNullOrWhiteSpace(phone) AndAlso System.Text.RegularExpressions.Regex.IsMatch(phone, "^\d{1,10}$")
+    End Function
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        ' Check if the click is on a button cell
+        If e.RowIndex >= 0 Then
+            Dim column As DataGridViewColumn = DataGridView1.Columns(e.ColumnIndex)
+
+            If column.HeaderText = "Action" OrElse column.HeaderText = "Edit" Then
+
+                If TypeOf column Is DataGridViewButtonColumn Then
+                    Dim buttonColumn As DataGridViewButtonColumn = DirectCast(column, DataGridViewButtonColumn)
+
+
+                    If buttonColumn.Text = "Delete" Then
+                        Dim selectedRow As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+                        Dim id As Integer = Convert.ToInt32(selectedRow.Cells("ID").Value)
+                        Dim name As String = Convert.ToString(selectedRow.Cells("FirstName").Value)
+                        DeleteEmployee(id)
+                    ElseIf buttonColumn.Text = "Edit" Then
+
+                        Dim selectedRow As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+                        Dim id As Integer = Convert.ToInt32(selectedRow.Cells("ID").Value)
+                        Dim name As String = Convert.ToString(selectedRow.Cells("FirstName").Value)
+                        Dim gender As String = Convert.ToString(selectedRow.Cells("Gender").Value)
+
+                        Select Case gender
+                            Case "Male"
+                                RadioButton1.Checked = True
+                            Case "Female"
+                                RadioButton2.Checked = True
+                            Case "Other"
+                                RadioButton3.Checked = True
+                            Case Else
+                                ' Handle any other cases if needed
+                        End Select
+
+                        TextBox5.Text = id
+                        TextBox1.Text = Convert.ToString(selectedRow.Cells("FirstName").Value)
+                        TextBox2.Text = Convert.ToString(selectedRow.Cells("LastName").Value)
+                        'RadioButton1.Text = Convert.ToString(selectedRow.Cells("Gender").Value)
+                        TextBox3.Text = Convert.ToString(selectedRow.Cells("Email").Value)
+                        TextBox4.Text = Convert.ToString(selectedRow.Cells("Contact").Value)
+                        DateTimePicker1.Text = Convert.ToDateTime(selectedRow.Cells("Dob").Value)
+                        DateTimePicker2.Text = Convert.ToDateTime(selectedRow.Cells("JoiningDate").Value)
+                        MessageBox.Show($"Edit clicked for ID: {id}, Name: {name}")
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+
+
+    Private Function DeleteEmployee(Id As Integer)
+
+        Using connection As New SQLiteConnection(connectionString)
+            connection.Open()
+
+            Dim sql As String = "DELETE FROM EmployeeDetail WHERE Id = @Id"
+
+            Using cmd As New SQLiteCommand(sql, connection)
+                cmd.Parameters.AddWithValue("@Id", Id)
+                cmd.ExecuteNonQuery()
+                MsgBox("Record Deleted Successfully.", MsgBoxStyle.MsgBoxRight, "Right")
+            End Using
+        End Using
+        LoadGridView()
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        LoadGridView()
+    End Sub
+
+    Function LoadGridView()
+        Dim sql As String = "SELECT * FROM EmployeeDetail"
+
+
+        Dim dataTable As New DataTable
+
+
+        Using connection As New SQLiteConnection(connectionString)
+            connection.Open()
+
+
+            Using adapter As New SQLiteDataAdapter(sql, connection)
+                adapter.Fill(dataTable)
+            End Using
+        End Using
+
+
+        Dim actionColumnExists As Boolean = False
+        For Each column As DataGridViewColumn In DataGridView1.Columns
+            If column.HeaderText = "Action" Then
+                actionColumnExists = True
+                Exit For
+            End If
+        Next
+
+        If Not actionColumnExists Then
+
+            Dim actionColumn As New DataGridViewButtonColumn()
+            actionColumn.HeaderText = "Action"
+            actionColumn.Text = "Delete"
+            actionColumn.UseColumnTextForButtonValue = True
+            DataGridView1.Columns.Add(actionColumn)
+
+            ' Add Edit button column
+            Dim editColumn As New DataGridViewButtonColumn()
+            editColumn.HeaderText = "Edit"
+            editColumn.Text = "Edit"
+            editColumn.UseColumnTextForButtonValue = True
+            DataGridView1.Columns.Add(editColumn)
+        End If
+
+
+        DataGridView1.DataSource = dataTable
+    End Function
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+
+    End Sub
+End Class
